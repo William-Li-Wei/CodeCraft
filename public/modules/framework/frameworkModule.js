3eee9e25a0888ec1982dff493ee071f2e0808947
William Lee <liweisadleader@hotmail.com>
2015-04-11 14:21:16 +0200

    update(framework): updated homepage controllers and pages.

--- public/modules/framework/frameworkModule.js
+++ public/modules/framework/frameworkModule.js
@@ -2,7 +2,7 @@
  * Created by william on 02.04.15.
  */
 
-var ccFramework = angular.module('cc.framework', []);
+var ccFramework = angular.module('cc.framework', ['duScroll', 'ngAnimate', 'ui.bootstrap', 'angular-loading-bar']);
 
 ccFramework.config(['$stateProvider', function($stateProvider) {
     $stateProvider
@@ -15,9 +15,38 @@ ccFramework.config(['$stateProvider', function($stateProvider) {
 
 ccFramework.controller('MainMenuController', ['$scope', function($scope) {
     $scope.menuOpened = false;
+    $scope.menu = {
+        buttons: [
+            {
+                name: '登录注册',
+                icon: 'sign-in'
+            }
+        ],
+        navigations: [
+            { name: '源艺首页', icon: 'home' },
+            { name: '专辑大厅', icon: 'institution'},
+            { name: '讨论专区', icon: 'comments-o' },
+            { name: '数据统计', icon: 'area-chart' },
+            { name: '关于本站', icon: 'sitemap' }
+        ]
+    }
+
+
+
+    // Interactions
     $scope.toggleMenu = function() {
         $scope.menuOpened = !$scope.menuOpened;
     }
+
+    // Menu Events
+    $scope.$on('cc-set-menu', function(buttons, navigations) {
+        if(buttons && buttons instanceof Array) {
+            $scope.menu.buttons = buttons;
+        }
+        if(navigations && navigations instanceof Array) {
+            $scope.menu.navigations = navigations;
+        }
+    });
 }]);
 
 ccFramework.controller('HomePageController', ['$scope', function($scope) {
