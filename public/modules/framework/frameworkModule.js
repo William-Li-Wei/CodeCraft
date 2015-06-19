90114f5b0b487ddb20275b88eac5f2ed5b18da4a
Wiliam Lee <william.lee@agrista.com>
2015-06-19 16:03:38 +0200

    add(sign-in-modal.html): added sign in modal page and it's controller.
    added a frameworkModalFactory to provide modal services.
    added controller for the signInModal.
    updated the nav-item in index.html with the click listener.
    
    added events to close the main menu.
    added events to reset the main menu.

--- public/modules/framework/frameworkModule.js
+++ public/modules/framework/frameworkModule.js
@@ -4,6 +4,10 @@
 
 var ccFramework = angular.module('cc.framework', ['duScroll', 'ngAnimate', 'ui.bootstrap', 'angular-loading-bar']);
 
+
+/**
+ * Config
+ */
 ccFramework.config(['$stateProvider', function($stateProvider) {
     $stateProvider
         .state('home', {
@@ -13,13 +17,21 @@ ccFramework.config(['$stateProvider', function($stateProvider) {
         });
 }]);
 
-ccFramework.controller('MainMenuController', ['$scope', function($scope) {
+
+/**
+ * Controllers
+ */
+ccFramework.controller('MainMenuController', ['$scope', 'frameworkModalFactory', function($scope, frameworkModalFactory) {
     $scope.menuOpened = false;
-    $scope.menu = {
+    var _defaultMenu = {
         buttons: [
             {
                 name: '登录注册',
-                icon: 'sign-in'
+                icon: 'sign-in',
+                click: function() {
+                    $scope.closeMenu();
+                    frameworkModalFactory.showSignInModal();
+                }
             }
         ],
         navigations: [
@@ -31,21 +43,32 @@ ccFramework.controller('MainMenuController', ['$scope', function($scope) {
         ]
     }
 
+    $scope.menu = angular.copy(_defaultMenu);
+
 
 
     // Interactions
     $scope.toggleMenu = function() {
         $scope.menuOpened = !$scope.menuOpened;
     }
+    $scope.closeMenu = function() {
+        $scope.menuOpened = false;
+    }
 
     // Menu Events
+    $scope.$on('cc-close-menu', function() {
+        $scope.closeMenu();
+    });
     $scope.$on('cc-set-menu', function(buttons, navigations) {
         if(buttons && buttons instanceof Array) {
-            $scope.menu.buttons = buttons;
+            $scope.menu.buttons = angular.copy(buttons);
         }
         if(navigations && navigations instanceof Array) {
-            $scope.menu.navigations = navigations;
+            $scope.menu.navigations = angular.copy(navigations);
         }
+    });
+    $scope.$on('cc-reset-menu', function(buttons, navigations) {
+        $scope.menu.buttons = angular.copy(_defaultMenu);
     });
 }]);
 
@@ -53,3 +76,36 @@ ccFramework.controller('HomePageController', ['$scope', function($scope) {
     $scope.pageConfig = pageConfig;
     $scope.language = 'chinese';
 }]);
+
+
+
+/**
+ * Modal Factory Services
+ */
+ccFramework.factory('frameworkModalFactory', ['$modal', function($modal) {
+    return {
+        showSignInModal: function() {
+            console.log('testing modal');
+            var modal = $modal.open({
+                templateUrl: '/modules/framework/sign-in-modal.html',
+                controller: 'SignInModalController'
+            });
+            return modal.result;
+        }
+    }
+}]);
+
+
+/**
+ * Modal Controllers
+ */
+ccFramework.controller('SignInModalController', ['$scope', '$modalInstance', 
+    function($scope, $modalInstance) {
+        // Interactions
+        $scope.close = function() {
+            $scope.cancel();
+        }
+        $scope.cancel = function() {
+            $modalInstance.dismiss('cancel');
+        }
+    }]);
